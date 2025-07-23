2e9646589a167ab939fb3f4f08dfa437833c655a
amanrent <amanrentc@gmail.com>
2025-07-23 10:34:23 +0700

    Commit

--- admin/api/get_artikel.php
+++ admin/api/get_artikel.php
@@ -1,10 +1,26 @@
 <?php
-
 include "../config.php";
 
 header('Content-Type: application/json');
 
-$query = "SELECT id, judul, isi, gambar, tanggal FROM artikel ORDER BY id DESC";
+$search = isset($_GET['search']) ? '%' . $conn->real_escape_string($_GET['search']) . '%' : null;
+$kategori = isset($_GET['kategori']) ? $conn->real_escape_string($_GET['kategori']) : null;
+
+$query = "SELECT id, judul, isi, gambar, tanggal, kategori FROM artikel";
+$conditions = [];
+
+if ($search) {
+  $conditions[] = "judul LIKE '$search' OR isi LIKE '$search'";
+}
+if ($kategori) {
+  $conditions[] = "kategori = '$kategori'";
+}
+
+if (!empty($conditions)) {
+  $query .= " WHERE " . implode(" AND ", $conditions);
+}
+
+$query .= " ORDER BY id DESC";
 $result = $conn->query($query);
 
 $artikel = [];
